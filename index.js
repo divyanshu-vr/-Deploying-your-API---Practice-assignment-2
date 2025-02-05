b669a10a904e4745bf0418b51b07f7b3a389fafe
Divyanshu Verma <73750407+divyanshuverma@users.noreply.github.com>
2025-02-05 15:10:30 +0530

    Added .env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 const express = require('express');
-const { resolve } = require('path');
 
 const app = express();
 const port = 3010;
@@ -7,9 +7,13 @@ const port = 3010;
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  if (process.env.IS_ADMIN === 'true') {
+    res.json({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.json({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`app listening at http://localhost:${port}`);
 });
